39ef6c9bfd5b1ee0a9a41a11a111aa85af6b7f57
CA <camilo.arguello.rincon@gmail.com>
2020-06-20 03:48:24 -0500

    [CA] Update Section projects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Home from './Components/Home';
 import Team from './Components/Team';
-
+import Work from './Components/Work';
 function App() {
   return (
     <div className="App">
       <Home />
       <Team />
+      <Work />
     </div>
   );
 }
